e61c95f08d9be56166b5106da38aae48a4ff219b
Nemanja Bojanic <nemanja.bojanic@pool4tool.com>
2018-01-12 19:30:15 +0100

    master router, components, bootstrap, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './components/layout/layout.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {BuyersComponentComponent} from './components/buyers-component/buyers-component.component';
+import {ProductsComponentComponent} from './components/products-component/products-component.component';
 
+
+const appRoutes: Routes = [
+    { path: '', component: ProductsComponentComponent },
+    { path: 'buyers', component: BuyersComponentComponent },
+    { path: 'products', component: ProductsComponentComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LayoutComponent,
+        NavbarComponent,
+        BuyersComponentComponent,
+        ProductsComponentComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
